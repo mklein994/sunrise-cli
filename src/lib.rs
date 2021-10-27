2e65f1077c12638d12f7926cae2bb5f3b299e626
Matthew Klein <mklein994@gmail.com>
2021-10-26 19:12:19 -0500

    Start exposing get_sunrise_sunset through wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 mod error;
 
+use std::str::FromStr;
+
 use chrono::prelude::*;
 use error::Error;
 use sunrise::Azimuth;
+use wasm_bindgen::prelude::*;
 
 /// Geographic coordinates
 pub struct Coord {
@@ -55,4 +58,42 @@ pub fn run(coord: Coord) {
             );
         };
     }
+}
+
+struct AzimuthWasm(Azimuth);
+
+impl FromStr for AzimuthWasm {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Official" => Ok(Self(Azimuth::Official)),
+            "Civil" => Ok(Self(Azimuth::Civil)),
+            "Nautical" => Ok(Self(Azimuth::Nautical)),
+            "Astronomical" => Ok(Self(Azimuth::Astronomical)),
+            _ => Err(format!("Invalid azimuth: {:?}", s)),
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_sunrise_sunset(
+    lat: f64,
+    lon: f64,
+    date: &str,
+    azimuth: &str,
+) -> Result<Vec<i64>, JsValue> {
+    let date = NaiveDate::parse_from_str(date, "%Y-%m-%d")
+        .map_err(|err| JsValue::from_str(&err.to_string()))?;
+
+    let (sunrise, sunset) = sunrise::time_of_transit(
+        lat,
+        lon,
+        date.year(),
+        date.month(),
+        date.day(),
+        azimuth.parse::<AzimuthWasm>()?.0,
+    );
+
+    Ok(vec![sunrise * 1000, sunset * 1000])
 }
