b2b0dbe9f104c0287c665ec2b14df2f22c57174f
Matthew Klein <mklein994@gmail.com>
2024-07-08 21:22:05 -0500

    Fix some clippy lints in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ const USAGE: &str = concat!("Usage: ", env!("CARGO_BIN_NAME"), " <lat> <lon>");
 
 fn main() {
     let coord = Coord::try_new().unwrap_or_else(|err| {
-        eprintln!("Error: {}", err);
-        eprintln!("{}", USAGE);
+        eprintln!("Error: {err}");
+        eprintln!("{USAGE}");
         std::process::exit(1);
     });
 
